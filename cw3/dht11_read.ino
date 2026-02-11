1d285c511ca843e9fd8e835fa53af56333253f59
Mugoya Brian <43634956+mugoyabrn@users.noreply.github.com>
2026-02-11 09:49:38 +0300

    Create dht11_read.ino

---
+++ cw3/dht11_read.ino
@@ -0,0 +1,22 @@
+#include <DHT.h>
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  Serial.print("Temp: ");
+  Serial.print(temp);
+  Serial.print(" C, Humidity: ");
+  Serial.println(hum);
+
+  delay(2000);
+}
